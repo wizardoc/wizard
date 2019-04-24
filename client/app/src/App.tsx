44a52b162e5ef67422a5b6002018b08d8211ec4b
HaoDaWang <1306036576@qq.com>
2019-04-24 13:29:03 +0800

    [Feat] clean

--- client/app/src/App.tsx
+++ client/app/src/App.tsx
@@ -1,22 +1,7 @@
-import * as React from 'react';
-import './App.css';
+import React, { Component } from "react";
 
-import logo from './logo.svg';
-
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+export class App extends Component {
+  render():React.ReactNode{
+    return <div>Hello Wizard</div>
   }
 }
-
-export default App;
--- client/app/src/index.tsx
+++ client/app/src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { App } from './App';
 
 ReactDOM.render(
   <App />,
