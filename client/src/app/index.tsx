2c5e9c5a5ea711523350e14c7931333b204c26b6
HaoDaWang <1306036576@qq.com>
2019-04-30 11:50:07 +0800

    [Feat] update version

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { App } from './App';
+import {App} from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 export * from './App';
