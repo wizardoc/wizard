d166267f80e6d3ab86bde6abfbea55862fa5f2c6
HaoDaWang <1306036576@qq.com>
2019-04-30 15:42:27 +0800

    [Feat] add theme of material theme

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,14 +1,18 @@
+import {MuiThemeProvider} from '@material-ui/core';
 import React, {Component} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
 import {AppRoutes} from './routes';
+import {theme} from './theme';
 
 export class App extends Component {
   render(): React.ReactNode {
     return (
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
