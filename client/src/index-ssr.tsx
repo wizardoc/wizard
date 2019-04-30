5cbfc00675944302f0bf66e2190a40af4adfc5dd
HaoDaWang <1306036576@qq.com>
2019-04-30 14:41:44 +0800

    [New] ssr module

---
+++ client/src/index-ssr.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import Loadable from 'react-loadable';
+import {Bundle, Manifest, getBundles} from 'react-loadable/webpack';
+import {StaticRouter} from 'react-router';
+
+import stats from '../build/react-loadable.json';
+
+// tslint:disable-next-line: import-path-shallowest
+import {AppRoutes} from './app/routes';
+
+interface SSRPayload {
+  html: string;
+  scripts: string[];
+}
+
+export class SSR {
+  render(url: string, data: unknown): SSRPayload {
+    const modules: string[] = [];
+    const context = {};
+    const html = ReactDOMServer.renderToString(
+      <Loadable.Capture report={moduleName => modules.push(moduleName)}>
+        <StaticRouter location={url} context={context}>
+          <AppRoutes initData={data} />
+        </StaticRouter>
+      </Loadable.Capture>,
+    );
+
+    const bundles: Bundle[] = getBundles(stats as Manifest, modules);
+
+    return {
+      html,
+      scripts: this.generateBundleScripts(bundles),
+    };
+  }
+
+  generateBundleScripts(bundles: Bundle[]): string[] {
+    return bundles
+      .filter(bundle => bundle.file.endsWith('.js'))
+      .map(
+        bundle =>
+          `<script type="text/javascript" src="${bundle.file}"></script>`,
+      );
+  }
+
+  static preloadAll(): Promise<void> {
+    return Loadable.preloadAll();
+  }
+}
