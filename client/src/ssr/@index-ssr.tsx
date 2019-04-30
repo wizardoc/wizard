98aaed59d7593948e84db9180cd21c7a19249a8b
HaoDaWang <1306036576@qq.com>
2019-04-30 15:20:00 +0800

    [Feat] stash ssr

--- client/src/ssr/@index-ssr.tsx
+++ client/src/ssr/@index-ssr.tsx
@@ -1,48 +1,48 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import Loadable from 'react-loadable';
-import {Bundle, getBundles} from 'react-loadable/webpack';
-import {StaticRouter} from 'react-router';
+// import React from 'react';
+// import ReactDOMServer from 'react-dom/server';
+// import Loadable from 'react-loadable';
+// import {Bundle, getBundles} from 'react-loadable/webpack';
+// import {StaticRouter} from 'react-router';
 
-import stats from '../../build/react-loadable.json';
-// tslint:disable-next-line: import-path-shallowest
-import {AppRoutes} from '../app/routes';
+// import stats from '../../build/react-loadable.json';
+// // tslint:disable-next-line: import-path-shallowest
+// import {AppRoutes} from '../app/routes';
 
-interface SSRPayload {
-  html: string;
-  scripts: string[];
-}
+// interface SSRPayload {
+//   html: string;
+//   scripts: string[];
+// }
 
-export class SSR {
-  render(url: string, data: unknown): SSRPayload {
-    const modules: string[] = [];
-    const context = {};
-    const html = ReactDOMServer.renderToString(
-      <Loadable.Capture report={moduleName => modules.push(moduleName)}>
-        <StaticRouter location={url} context={context}>
-          <AppRoutes initData={data} />
-        </StaticRouter>
-      </Loadable.Capture>,
-    );
+// export class SSR {
+//   render(url: string, data: unknown): SSRPayload {
+//     const modules: string[] = [];
+//     const context = {};
+//     const html = ReactDOMServer.renderToString(
+//       <Loadable.Capture report={moduleName => modules.push(moduleName)}>
+//         <StaticRouter location={url} context={context}>
+//           <AppRoutes initData={data} />
+//         </StaticRouter>
+//       </Loadable.Capture>,
+//     );
 
-    const bundles: Bundle[] = getBundles(stats as any, modules);
+//     const bundles: Bundle[] = getBundles(stats as any, modules);
 
-    return {
-      html,
-      scripts: this.generateBundleScripts(bundles),
-    };
-  }
+//     return {
+//       html,
+//       scripts: this.generateBundleScripts(bundles),
+//     };
+//   }
 
-  generateBundleScripts(bundles: Bundle[]): string[] {
-    return bundles
-      .filter(bundle => bundle.file.endsWith('.js'))
-      .map(
-        bundle =>
-          `<script type="text/javascript" src="${bundle.file}"></script>`,
-      );
-  }
+//   generateBundleScripts(bundles: Bundle[]): string[] {
+//     return bundles
+//       .filter(bundle => bundle.file.endsWith('.js'))
+//       .map(
+//         bundle =>
+//           `<script type="text/javascript" src="${bundle.file}"></script>`,
+//       );
+//   }
 
-  static preloadAll(): Promise<void> {
-    return Loadable.preloadAll();
-  }
-}
+//   static preloadAll(): Promise<void> {
+//     return Loadable.preloadAll();
+//   }
+// }
