32b80acca7aaa6b4141b97f86d7ac505af684aae
HaoDaWang <1306036576@qq.com>
2019-05-05 22:54:40 +0800

    [Feat] add about

--- client/src/app/routes/app-route.tsx
+++ client/src/app/routes/app-route.tsx
@@ -1,6 +1,7 @@
 import React, {FunctionComponent, ReactElement} from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
 
+import {About} from '../pages/about';
 import {Doc} from '../pages/doc';
 import {Home} from '../pages/home';
 import {PageNotFound} from '../pages/page-not-found';
@@ -16,6 +17,7 @@ export const AppRoutes: FunctionComponent<AppRoutesProps> = (
     <Route exact path="/" render={() => <Redirect to="/home" />} />
     <Route exact path="/home" component={Home} />
     <Route exact path="/doc" component={Doc} />
+    <Route exact path="/about" component={About} />
     <Route component={PageNotFound} />
   </Switch>
 );
