3836c7f27ae5dae05345b722e5491785f4e7c375
HaoDaWang <1306036576@qq.com>
2019-05-06 14:03:29 +0800

    [Feat] fix css

--- client/src/app/ui/button.tsx
+++ client/src/app/ui/button.tsx
@@ -5,6 +5,6 @@ export const ActionButton = styled(Button)`
   border-radius: 3px;
   border: 0;
   height: 48px;
-  padding: '0 30px';
-  box-shadow: '0 3px 5px 2px rgba(255, 105, 135, .3)';
+  padding: 0 30px !important;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3) !important;
 `;
