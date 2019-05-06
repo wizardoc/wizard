344a0ee1bb40474dcac40c1645bd05da2e1c32c5
HaoDaWang <1306036576@qq.com>
2019-05-06 20:15:03 +0800

    [Feat] register

--- client/src/app/components/@register.tsx
+++ client/src/app/components/@register.tsx
@@ -1,7 +1,39 @@
+import {TextField} from '@material-ui/core';
 import React, {Component, ReactNode} from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 24px !important;
+  padding-bottom: 34px !important;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+// TextFieldProps
+const TextFieldWrapper = styled(TextField)<any>`
+  width: 70%;
+  margin: 0 50px;
+  margin-top: 15px !important;
+`;
 
 export class Register extends Component {
   render(): ReactNode {
-    return <div>Register</div>;
+    return (
+      <Wrapper>
+        <TextFieldWrapper
+          label="账号"
+          type="text"
+          autoComplete="new-password"
+        />
+        <TextFieldWrapper
+          label="密码"
+          type="password"
+          autoComplete="new-password"
+        />
+      </Wrapper>
+    );
   }
 }
