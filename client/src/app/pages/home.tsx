ad83b07ec130a0fdba64808dabcb8e01f7d0a201
HaoDaWang <1306036576@qq.com>
2019-05-06 15:14:24 +0800

    [Feat] wrap home

--- client/src/app/pages/home.tsx
+++ client/src/app/pages/home.tsx
@@ -1,7 +1,19 @@
 import React, {Component, ReactNode} from 'react';
+// tslint:disable-next-line:import-path-shallowest
+import styled from 'styled-components';
+
+import {Started} from '../components';
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
 
 export class Home extends Component {
   render(): ReactNode {
-    return <div>Home</div>;
+    return (
+      <Wrapper>
+        <Started />
+      </Wrapper>
+    );
   }
 }
