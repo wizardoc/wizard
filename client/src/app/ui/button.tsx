c63a8a649eda0065d1c16bc11ec36b5b71d1e4c2
HaoDaWang <1306036576@qq.com>
2019-05-06 13:55:54 +0800

    [Feat] export button

---
+++ client/src/app/ui/button.tsx
@@ -0,0 +1,10 @@
+import {Button} from '@material-ui/core';
+import styled from 'styled-components';
+
+export const ActionButton = styled(Button)`
+  border-radius: 3px;
+  border: 0;
+  height: 48px;
+  padding: '0 30px';
+  box-shadow: '0 3px 5px 2px rgba(255, 105, 135, .3)';
+`;
