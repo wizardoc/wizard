3c5e4c6f4393649b8d139ec7fb3f536b9fb1bae8
HaoDaWang <1306036576@qq.com>
2019-05-06 15:15:01 +0800

    [Feat] update color to white

--- client/src/app/ui/button.tsx
+++ client/src/app/ui/button.tsx
@@ -5,6 +5,7 @@ export const ActionButton = styled(Button)`
   border-radius: 3px;
   border: 0;
   height: 48px;
+  color: white !important;
   padding: 0 30px !important;
   box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3) !important;
 `;
