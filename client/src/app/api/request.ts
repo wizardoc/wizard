0d3ee44acf4b644a6ff09d63726b69e96d00ddf8
HaoDaWang <1306036576@qq.com>
2019-05-13 19:02:40 +0800

    [Feat] add request wrap

---
+++ client/src/app/api/request.ts
@@ -0,0 +1,17 @@
+import Axios from 'axios';
+import {Injectable} from 'react-ts-di';
+
+import ServerConfig from '../configs/server-config.json';
+
+const {baseUrl, port} = ServerConfig;
+
+export interface PostPayload<T = unknown> {
+  [index: number]: T;
+}
+
+@Injectable()
+export class Request {
+  post<T>(url: string, data: PostPayload<T>) {
+    // Axios.post(url, data);
+  }
+}
