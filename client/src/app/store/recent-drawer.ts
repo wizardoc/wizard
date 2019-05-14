163c641c395d6890c7ff49dba4b4c9bf5242a37d
HaoDaWang <1306036576@qq.com>
2019-05-14 22:15:25 +0800

    [Feat] add recent drawer store

---
+++ client/src/app/store/recent-drawer.ts
@@ -0,0 +1,11 @@
+import {action, observable} from 'mobx';
+
+export class RecentDrawer {
+  @observable
+  isViewRencentDrawer = false;
+
+  @action
+  viewRecentDrawerToggle(): void {
+    this.isViewRencentDrawer = !this.isViewRencentDrawer;
+  }
+}
