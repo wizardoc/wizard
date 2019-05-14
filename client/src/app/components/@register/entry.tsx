af0b564d4db0ac5e25da434b95ae3af4fa763571
HaoDaWang <1306036576@qq.com>
2019-05-14 11:58:11 +0800

    [Feat] add tab change logic

--- client/src/app/components/@register/entry.tsx
+++ client/src/app/components/@register/entry.tsx
@@ -1,13 +1,19 @@
 import {Button, Step, StepLabel, Stepper} from '@material-ui/core';
 import {StepperProps} from '@material-ui/core/Stepper';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import CloseIcon from '@material-ui/icons/Close';
+import {action, observable} from 'mobx';
 import {observer} from 'mobx-react';
 import React, {Component, ComponentType, ReactNode} from 'react';
 import styled from 'styled-components';
 
 import {AccessDialogStore} from '../../store';
 import {InjectStore} from '../../utils';
+
+import {BaseInfo} from './base-info';
+import {Complete} from './complete';
+import {Origanization} from './origanization';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -42,21 +48,44 @@ export class Register extends Component {
   @InjectStore(AccessDialogStore)
   private accessDialogStore!: AccessDialogStore;
 
+  @observable
+  private currentIndex = 0;
+
+  private registerBody = [<BaseInfo />, <Complete />, <Origanization />];
+
+  @action
+  nextStepToggle(): void {
+    this.currentIndex += 1;
+  }
+
+  @action
+  preStepToggle(): void {
+    this.currentIndex -= 1;
+  }
+
   handleCloseClick(): void {
     this.accessDialogStore.accessDialogToggle();
+  }
+
+  handleNextClick(): void {
+    this.nextStepToggle();
+  }
+
+  handlePreClick(): void {
+    this.preStepToggle();
   }
 
   render(): ReactNode {
     return (
       <Wrapper>
-        <StepperWrapper>
-          {steps.map(label => (
-            <Step key={label}>
+        <StepperWrapper activeStep={this.currentIndex}>
+          {steps.map((label, index) => (
+            <Step key={label} completed={index < this.currentIndex}>
               <StepLabel>{label}</StepLabel>
             </Step>
           ))}
         </StepperWrapper>
-
+        {this.registerBody[this.currentIndex]}
         <Row>
           <ButtonsWrapper>
             <Button
@@ -67,7 +96,21 @@ export class Register extends Component {
               取消
               <CloseIcon />
             </Button>
-            <Button variant="contained" color="primary">
+            {this.currentIndex > 0 && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => this.handlePreClick()}
+              >
+                <ArrowBackIcon />
+                上一步
+              </Button>
+            )}
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => this.handleNextClick()}
+            >
               下一步
               <ArrowForwardIcon />
             </Button>
