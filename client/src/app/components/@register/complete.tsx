634b317ff6ba1b2c19add885aadffb784261d4be
HaoDaWang <1306036576@qq.com>
2019-05-23 22:25:13 +0800

    feat: complete

--- client/src/app/components/@register/complete.tsx
+++ client/src/app/components/@register/complete.tsx
@@ -1,7 +1,18 @@
 import React, {Component, ReactNode} from 'react';
+import styled from 'styled-components';
+
+import RegisterSuccess from '../../assets/static/register_successful.png';
+
+const RegisterSuccessImg = styled.img`
+  width: 300px;
+`;
 
 export class Complete extends Component {
   render(): ReactNode {
-    return <div />;
+    return (
+      <div>
+        <RegisterSuccessImg src={RegisterSuccess} />
+      </div>
+    );
   }
 }
