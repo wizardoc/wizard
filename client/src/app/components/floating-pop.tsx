7425dabe3417fa920ecd9083e0ce5b55172d4c04
HaoDaWang <1306036576@qq.com>
2019-05-25 16:04:55 +0800

    [Feat] add speed dail instead of floating btn

--- client/src/app/components/floating-pop.tsx
+++ client/src/app/components/floating-pop.tsx
@@ -1,9 +1,12 @@
-import {Fab} from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
 import MemoryIcon from '@material-ui/icons/Memory';
+import MessageIcon from '@material-ui/icons/Message';
+import PersonIcon from '@material-ui/icons/Person';
 import SpeedDial from '@material-ui/lab/SpeedDial';
 import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
-import {observable} from 'mobx';
+import {action, observable} from 'mobx';
 import {observer} from 'mobx-react';
 import React, {Component, ReactNode} from 'react';
 import styled from 'styled-components';
@@ -38,35 +41,45 @@ const IconWrapper = styled.div<MenuIconWrapperProps>`
   transform: ${props => (props.isRotate ? 'rotate(405deg)' : 'rotate(0deg)')};
 `;
 
+// const FunctionBar = styled(SpeedDial)`
+//   > .MuiButtonBase-root {
+//     background: ${props => props.theme.secondaryColor};
+//   }
+// ` as ComponentType<SpeedDialProps>;
+
 @observer
 export class FloatingPop extends Component {
   private actions: FloatActions[] = [
-    {name: 'write', icon: <EditIcon />},
-    {name: 'profile', icon: <EditIcon />},
+    {name: '新规范', icon: <EditIcon />},
+    {name: '个人信息', icon: <PersonIcon />},
+    {name: '邀请新的组织成员', icon: <GroupAddIcon />},
+    {name: '消息', icon: <MessageIcon />},
+    {name: '创建新组织', icon: <LocationCityIcon />},
   ];
 
   @observable
   private isOpenMenu = false;
 
-  handleFloatingPopClick(): void {
+  @action
+  menuToggle(): void {
     this.isOpenMenu = !this.isOpenMenu;
   }
 
   render(): ReactNode {
     return (
       <Wrapper>
-        <Fab
-          color="secondary"
-          aria-label="Add"
-          onMouseOver={() => this.handleFloatingPopClick()}
-        >
-          <IconWrapper isRotate={this.isOpenMenu}>
-            <MemoryIcon />
-          </IconWrapper>
-        </Fab>
         <SpeedDial
+          ButtonProps={{
+            color: 'secondary',
+          }}
           ariaLabel="抽屉"
-          icon={<MemoryIcon />}
+          icon={
+            <IconWrapper isRotate={this.isOpenMenu}>
+              <MemoryIcon />
+            </IconWrapper>
+          }
+          onMouseEnter={() => this.menuToggle()}
+          onMouseLeave={() => this.menuToggle()}
           open={this.isOpenMenu}
         >
           {this.actions.map(({name, icon, tooltipTitle}) => (
