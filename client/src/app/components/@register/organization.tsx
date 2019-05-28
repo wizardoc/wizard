60171d486c37c66ac5038a0be08ed1d8daf4fb6a
HaoDaWang <1306036576@qq.com>
2019-05-28 14:04:36 +0800

    [Feat] update tip

--- client/src/app/components/@register/organization.tsx
+++ client/src/app/components/@register/organization.tsx
@@ -1,12 +1,9 @@
-import {
-  SnackbarContent,
-  Typography,
-  WithStyles,
-  withStyles,
-} from '@material-ui/core';
+import {Typography, WithStyles, withStyles} from '@material-ui/core';
 import {StyleRules, createStyles} from '@material-ui/core/styles';
 import React, {Component, ReactNode} from 'react';
 import styled from 'styled-components';
+
+import {TipContent, TipVariant} from '../../ui';
 
 const Wrapper = styled.div``;
 
@@ -20,13 +17,14 @@ export class TOrganization extends Component<OrganizationProps> {
 
     return (
       <Wrapper>
-        <SnackbarContent
+        <TipContent
+          tipVariant={TipVariant.Main}
           message={
             <>
               <Typography variant="h6" component="h3">
                 选择一个组织，开始你的规范日程
               </Typography>
-              <Typography>组织是 wizard 的顶层建筑，文档由组织维护</Typography>
+              <Typography>组织是 wizard 的顶层建筑，规范由组织维护</Typography>
             </>
           }
         />
