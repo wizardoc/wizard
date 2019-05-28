c66ccd52f7df354d10e3b829ec1d129bdabfcab0
HaoDaWang <1306036576@qq.com>
2019-05-28 13:29:09 +0800

    [Feat] wrap tip

--- client/src/app/ui/tip.tsx
+++ client/src/app/ui/tip.tsx
@@ -1,9 +1,14 @@
-import {SnackbarContent} from '@material-ui/core';
+import {SnackbarContent, IconButton} from '@material-ui/core';
 import Snackbar from '@material-ui/core/Snackbar';
 import {SnackbarContentProps} from '@material-ui/core/SnackbarContent';
 import {observer} from 'mobx-react';
-import React, {Component, ComponentType, ReactNode} from 'react';
+import React, {Component, ComponentType, ReactNode, MouseEvent} from 'react';
 import styled from 'styled-components';
+import InfoIcon from '@material-ui/icons/Info';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import WarningIcon from '@material-ui/icons/Warning';
+import ErrorIcon from '@material-ui/icons/Error';
+import CloseIcon from '@material-ui/icons/Close';
 
 import {TipStore} from '../store';
 import {InjectStore} from '../utils';
@@ -18,20 +23,51 @@ export const enum TipVariant {
 interface TipProps extends SnackbarContentProps {
   tipVariant: TipVariant;
   message: ReactNode;
-  hasCloseClick?: boolean;
+  onClose?(e?: MouseEvent): void;
 }
 
 interface TipContentProps {
-  tipvariant: TipVariant;
+  bgcolor: string;
 }
 
-const colors = ['#43a047', 'rgb(97, 136, 51)', '#ffa000', '#d32f2f'];
+interface Stuff {
+  color: string;
+  icon: unknown;
+}
+
+const stuffs: Stuff[] = [
+  {
+    color: '#43a047',
+    icon: <CheckCircleIcon />,
+  },
+  {
+    color: 'rgb(97, 136, 51)',
+    icon: <InfoIcon />,
+  },
+  {
+    color: '#ffa000',
+    icon: <WarningIcon />,
+  },
+  {
+    color: '#d32f2f',
+    icon: <ErrorIcon />,
+  },
+];
 
 const TipContent = styled(SnackbarContent)<TipContentProps>`
-  background-color: ${props => colors[props.tipvariant]} !important;
+  background-color: ${({bgcolor}) => bgcolor} !important;
 ` as ComponentType<SnackbarContentProps & TipContentProps>;
 
-const MAX_EXIST_DURATION = 60 * 1000;
+const MessageContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Message = styled.div`
+  margin-left: 10px;
+`;
+
+const MAX_EXIST_DURATION = 5 * 1000;
 
 @observer
 export class Tip extends Component<TipProps> {
@@ -39,20 +75,46 @@ export class Tip extends Component<TipProps> {
   private tipStore!: TipStore;
 
   render(): ReactNode {
-    const {message, tipVariant} = this.props;
+    const {message, tipVariant, onClose} = this.props;
+    const {color, icon} = stuffs[tipVariant];
+    const actions = [];
 
-    console.info(colors[tipVariant]);
+    if (onClose) {
+      actions.push(
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          onClick={e => {
+            onClose(e);
+            this.tipStore.tipToggle();
+          }}
+        >
+          <CloseIcon />
+        </IconButton>,
+      );
+    }
+
+    setTimeout(() => this.tipStore.tipToggle(), MAX_EXIST_DURATION);
 
     return (
       <Snackbar
         open={this.tipStore.isShowTip}
-        autoHideDuration={MAX_EXIST_DURATION}
         anchorOrigin={{
           vertical: 'bottom',
           horizontal: 'left',
         }}
       >
-        <TipContent tipvariant={tipVariant} message={message} />
+        <TipContent
+          bgcolor={color}
+          message={
+            <MessageContent>
+              {icon}
+              <Message>{message}</Message>
+            </MessageContent>
+          }
+          action={actions}
+        />
       </Snackbar>
     );
   }
