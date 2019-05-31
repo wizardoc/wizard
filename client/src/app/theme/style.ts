6517ff97676ba3f642e0a6f56b9155d0805b42f7
HaoDaWang <1306036576@qq.com>
2019-05-31 20:36:21 +0800

    [Feat] add spacing

--- client/src/app/theme/style.ts
+++ client/src/app/theme/style.ts
@@ -22,6 +22,7 @@ export const styledTheme = {
   highRed: '#e91e63',
   shallowGray: '#eeeeee',
   redLinearGradient: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  spacing: '10px',
 };
 
 export default styled;
