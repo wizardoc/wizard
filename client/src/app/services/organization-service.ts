bb7399f479a2ed33232e79c31aaa1934d903eca5
HaoDaWang <1306036576@qq.com>
2019-06-09 18:01:22 +0800

    [Feat] add createOrganization

--- client/src/app/services/organization-service.ts
+++ client/src/app/services/organization-service.ts
@@ -14,14 +14,27 @@ export class Organization {
   private organizationScope = 'organization';
 
   async getAllNames(): Promise<string[]> {
-    const {
-      data: {organizeNames},
-    } = await this.request.get<OrganizationNames>(this.parseUrl('/name/all'));
+    const {organizeNames} = await this.request.get<OrganizationNames>(
+      this.parseUrl('/name/all'),
+    );
 
     return organizeNames;
   }
 
+  createOrganization(name: string, description: string): void {
+    this.request.post<{}, string>(this.parseUrl('/new'), {
+      name,
+      description,
+    });
+  }
+
+  async hasExistOrganization(organizationName: string): Promise<boolean> {
+    const names = await this.getAllNames();
+
+    return names.includes(organizationName);
+  }
+
   private parseUrl(path: string): string {
-    return `${this.organizationScope}/${path}`;
+    return `${this.organizationScope}${path}`;
   }
 }
