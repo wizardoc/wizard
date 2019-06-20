2017179ca018871cbadeacfb600a6c2b8aa98337
HaoDaWang <1306036576@qq.com>
2019-06-20 11:32:45 +0800

    [Feat] add requestType interceptor

--- client/src/app/api/@interceptors.ts
+++ client/src/app/api/@interceptors.ts
@@ -1,5 +1,23 @@
-import {AxiosResponse} from 'axios';
+import {AxiosRequestConfig, AxiosResponse} from 'axios';
+import * as Qs from 'qs';
+
+import {ContentType} from './request';
 
 export function getData<T = unknown>(res: AxiosResponse<T>): T {
   return res.data;
+}
+
+export function requestType(
+  config: AxiosRequestConfig,
+): AxiosRequestConfig | Promise<AxiosRequestConfig> {
+  console.info(config);
+  const dup = {...config};
+  const {data} = dup;
+  const contentType = config.headers['Content-Type'];
+
+  if (contentType === ContentType.Form) {
+    dup.data = Qs.stringify(data);
+  }
+
+  return dup;
 }
