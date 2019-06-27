c45ab984bf778c86a4ac8cbd5d10a747d42a6f9b
xiaoyufei <915724526@qq.com>
2019-06-27 22:36:24 +0800

    [Feat]: github-btn

--- client/src/app/components/main/@github-btn.tsx
+++ client/src/app/components/main/@github-btn.tsx
@@ -66,6 +66,51 @@ const GithubText = styled.span`
   line-height: 22px;
 `;
 
+const CountContainer = styled.a`
+  padding: 3px 10px 3px 8px;
+  font-size: 16px;
+  line-height: 22px;
+  border-radius: 4px;
+  display: block;
+  margin-left: 6px;
+  background-color: #fafafa;
+  border: 1px solid #d4d4d4;
+  color: #333;
+  text-decoration: none;
+  white-space: nowrap;
+  cursor: pointer;
+  position: relative;
+  &:before {
+    left: -5px;
+    margin-top: -6px;
+    border-width: 6px 6px 6px 0;
+    top: 50%;
+    content: '';
+    position: absolute;
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border-color: transparent;
+    border-style: solid;
+    border-right-color: #fafafa;
+  }
+  &:after {
+    left: -6px;
+    margin-top: -7px;
+    border-width: 7px 7px 7px 0;
+    top: 50%;
+    z-index: -1;
+    content: '';
+    position: absolute;
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border-color: transparent;
+    border-style: solid;
+    border-right-color: #d4d4d4;
+  }
+`;
+
 export class GithubBtn extends Component {
   render(): ReactNode {
     return (
@@ -74,6 +119,12 @@ export class GithubBtn extends Component {
           <GithubIcon />
           <GithubText>Star</GithubText>
         </StarContainer>
+        <CountContainer
+          target="_blank"
+          href="//github.com/wizaaard/wizard/stargazers/"
+        >
+          123
+        </CountContainer>
       </Container>
     );
   }
