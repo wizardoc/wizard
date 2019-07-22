e4f0dc5b3f02bfebcb7c9d1942fcb65ef1352f0c
HaoDaWang <1306036576@qq.com>
2019-07-22 13:44:43 +0800

    [Feat] add baseBgColor

--- client/src/app/theme/style.ts
+++ client/src/app/theme/style.ts
@@ -23,6 +23,7 @@ export const styledTheme = {
   shallowGray: '#eeeeee',
   redLinearGradient: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   spacing: '10px',
+  baseBgColor: '#eceff1',
 };
 
 export default styled;
