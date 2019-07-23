589cdf851d6f3a8f50e9cb823f42b2c7b781bf51
HaoDaWang <1306036576@qq.com>
2019-07-23 10:34:57 +0800

    [Feat] wrap storage

---
+++ client/src/app/utils/storage.ts
@@ -0,0 +1,40 @@
+class BaseStorage {
+  private storage: Storage;
+
+  constructor(storage: Storage) {
+    this.storage = storage;
+  }
+
+  setItem(key: string, data: unknown): void {
+    this.storage.setItem(key, JSON.stringify(data));
+  }
+
+  getItem<T>(key: string): T | undefined {
+    const result = this.storage.getItem(key);
+
+    if (result === '' || result === null) {
+      return undefined;
+    }
+
+    return JSON.parse(result);
+  }
+
+  removeItem(key: string): void {
+    this.storage.removeItem(key);
+  }
+
+  key(index: number): string | null {
+    return this.storage.key(index);
+  }
+
+  clear(): void {
+    return this.storage.clear();
+  }
+
+  get length(): number {
+    return this.storage.length;
+  }
+}
+
+export const LocalStorage = new BaseStorage(localStorage);
+export const SessionStorage = new BaseStorage(sessionStorage);
