c0f12fc6fe862cc98fd63930abff23035ccfb5da
HaoDaWang <1306036576@qq.com>
2019-07-27 14:11:59 +0800

    [Feat] add card of tip

--- client/src/app/pages/register-page.tsx
+++ client/src/app/pages/register-page.tsx
@@ -7,10 +7,19 @@ import {AccessBox, Wrapper} from './common-style-component/access-wrapper';
 
 const RegisterBox = styled(AccessBox)``;
 
+const TipCard = styled.div`
+  width: 300px;
+  height: 512px;
+  background: ${props => props.theme.primaryColor};
+  border-radius: 10px;
+  margin-right: 20px;
+`;
+
 export class RegisterPage extends Component {
   render(): ReactNode {
     return (
       <Wrapper>
+        <TipCard></TipCard>
         <RegisterBox>
           <Register />
         </RegisterBox>
