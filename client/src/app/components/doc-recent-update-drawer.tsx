858a8264cc13a0734539135e56fce4597c8f2611
HaoDaWang <1306036576@qq.com>
2019-07-28 16:25:14 +0800

    [Feat] extract drawerHeader

--- client/src/app/components/doc-recent-update-drawer.tsx
+++ client/src/app/components/doc-recent-update-drawer.tsx
@@ -1,25 +1,13 @@
-import {
-  Divider,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  SwipeableDrawer,
-} from '@material-ui/core';
-import {ListItemIconProps} from '@material-ui/core/ListItemIcon';
+import {List, SwipeableDrawer} from '@material-ui/core';
 import PetsIcon from '@material-ui/icons/Pets';
 import {observer} from 'mobx-react';
-import React, {Component, ComponentType, ReactNode} from 'react';
-import styled from 'styled-components';
+import React, {Component, ReactNode} from 'react';
 
 import {RecentDrawer} from '../store';
+import {DrawerHeader} from '../ui';
 import {InjectStore} from '../utils';
 
 const HEAD_PROMPT = '显示近期的文档动态（保留 30 天）';
-
-const PrimaryPetsIconItem = styled(ListItemIcon)`
-  color: ${props => props.theme.primaryColor} !important;
-` as ComponentType<ListItemIconProps>;
 
 @observer
 export class DocRecentUpdateDrawer extends Component {
@@ -36,13 +24,7 @@ export class DocRecentUpdateDrawer extends Component {
         onClose={viewRecentDrawerToggle}
       >
         <List>
-          <ListItem>
-            <PrimaryPetsIconItem>
-              <PetsIcon />
-            </PrimaryPetsIconItem>
-            <ListItemText primary={HEAD_PROMPT} />
-          </ListItem>
-          <Divider />
+          <DrawerHeader title={HEAD_PROMPT} icon={<PetsIcon />}></DrawerHeader>
         </List>
       </SwipeableDrawer>
     );
