bc10b243a81030457d1062f5d5a1b5be30f25da8
HaoDaWang <1306036576@qq.com>
2019-08-06 16:41:35 +0800

    [Feat] add header of page

--- client/src/app/pages/about.tsx
+++ client/src/app/pages/about.tsx
@@ -1,11 +1,69 @@
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
 import React, {Component, ReactNode} from 'react';
+import {Inject} from 'react-ts-di';
+import styled from 'styled-components';
 
 import {RouterAnimation} from '../animations';
+import {PageHeader, TreeViewGenerator} from '../components';
+import {DocService} from '../services';
+import {marked} from '../utils';
 
-export const About = RouterAnimation(
-  class extends Component {
-    render(): ReactNode {
-      return <div>about</div>;
-    }
-  },
-);
+const Wrapper = styled.div``;
+
+const Side = styled.div`
+  min-width: 290px;
+  margin-right: 25px;
+  padding: 10px;
+  min-height: 500px;
+  background: rgba(0, 0, 0, 0.04);
+  color: rgba(0, 0, 0, 0.87) !important;
+`;
+
+const SideTitle = styled.p`
+  font-size: 30px;
+  font-weight: 300;
+`;
+
+const Content = styled.div``;
+const PageContent = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 25px;
+  box-sizing: border-box;
+`;
+
+@observer
+class TAbout extends Component {
+  @Inject
+  docService!: DocService;
+
+  @observable
+  content: string = '';
+
+  render(): ReactNode {
+    return (
+      <Wrapper>
+        <PageHeader title="关于"></PageHeader>
+        <PageContent>
+          <Side>
+            <SideTitle>目录</SideTitle>
+            <TreeViewGenerator
+              rootText="关于"
+              content={this.content}
+            ></TreeViewGenerator>
+          </Side>
+          <Content
+            dangerouslySetInnerHTML={{__html: marked(this.content)}}
+          ></Content>
+        </PageContent>
+      </Wrapper>
+    );
+  }
+
+  async componentDidMount(): Promise<void> {
+    this.content = await this.docService.getAboutWizard();
+  }
+}
+
+export const About = RouterAnimation(TAbout);
--- client/src/app/pages/doc.tsx
+++ client/src/app/pages/doc.tsx
@@ -1,11 +1,22 @@
 import React, {Component, ReactNode} from 'react';
+import styled from 'styled-components';
 
 import {RouterAnimation} from '../animations';
+import {PageHeader} from '../components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+`;
 
 export const Doc = RouterAnimation(
   class extends Component {
     render(): ReactNode {
-      return <div>Doc</div>;
+      return (
+        <Wrapper>
+          <PageHeader title="文档"></PageHeader>
+        </Wrapper>
+      );
     }
   },
 );
