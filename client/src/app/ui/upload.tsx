643473cf525575f20e91c76801ac8c3a3b1d94b5
HaoDaWang <1306036576@qq.com>
2019-08-06 16:40:39 +0800

    [Feat] remove image judge logic

--- client/src/app/ui/upload.tsx
+++ client/src/app/ui/upload.tsx
@@ -1,9 +1,5 @@
 import React, {ChangeEvent, Component, ReactNode, createRef} from 'react';
-import {Inject} from 'react-ts-di';
 import styled from 'styled-components';
-
-import {Toast} from '../services';
-import {isImage} from '../utils';
 
 interface FileInputable {
   files: FileList;
@@ -21,9 +17,6 @@ const UploadInput = styled.input`
 `;
 
 export class Upload extends Component<UploadProps> {
-  @Inject
-  private toast!: Toast;
-
   private uploadRef = createRef<HTMLInputElement>();
 
   handleUploadChange({target}: ChangeEvent): void {
@@ -31,14 +24,6 @@ export class Upload extends Component<UploadProps> {
     const fileInputable = (target as unknown) as FileInputable;
     const {files} = fileInputable;
     const file = files[0];
-    const {type} = file;
-
-    if (!isImage(type)) {
-      this.toast.error('请上传 png/ jpg 格式的头像');
-      fileInputable.value = '';
-
-      return;
-    }
 
     onAfterRead(file);
   }
