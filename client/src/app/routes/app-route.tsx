834755d5c920183fd6fdc61ca0fd457b527cc95d
HaoDaWang <1306036576@qq.com>
2019-08-06 19:27:54 +0800

    [Feat] listen history

--- client/src/app/routes/app-route.tsx
+++ client/src/app/routes/app-route.tsx
@@ -1,5 +1,11 @@
-import React, {FunctionComponent, ReactElement} from 'react';
-import {Redirect, Route, Switch} from 'react-router-dom';
+import React, {Component, ReactNode} from 'react';
+import {
+  Redirect,
+  Route,
+  RouteComponentProps,
+  Switch,
+  withRouter,
+} from 'react-router-dom';
 
 import {About} from '../pages/about';
 import {Doc} from '../pages/doc';
@@ -7,21 +13,56 @@ import {Home} from '../pages/home';
 import {LoginPage} from '../pages/login-page';
 import {PageNotFound} from '../pages/page-not-found';
 import {RegisterPage} from '../pages/register-page';
+import {UIControl} from '../store';
+import {InjectStore} from '../utils';
 
 interface AppRoutesProps {
   initData?: unknown;
 }
 
-export const AppRoutes: FunctionComponent<AppRoutesProps> = (
-  _initData?: unknown,
-): ReactElement => (
-  <Switch>
-    <Route exact path="/" render={() => <Redirect to="/home" />} />
-    <Route exact path="/home" component={Home} />
-    <Route exact path="/doc" component={Doc} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/register" component={RegisterPage} />
-    <Route exact path="/login" component={LoginPage} />?
-    <Route component={PageNotFound} />
-  </Switch>
-);
+export class TAppRoutes extends Component<
+  AppRoutesProps & RouteComponentProps
+> {
+  @InjectStore(UIControl)
+  uiControl!: UIControl;
+
+  private unListen: Function | undefined;
+
+  render(): ReactNode {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/doc" component={Doc} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/register" component={RegisterPage} />
+        <Route exact path="/login" component={LoginPage} />?
+        <Route component={PageNotFound} />
+      </Switch>
+    );
+  }
+
+  componentDidMount(): void {
+    const {history} = this.props;
+
+    this.unListen = history.listen(loc => {
+      const {pathname} = loc;
+
+      if (pathname === '/home') {
+        this.uiControl.isMainPage = true;
+      } else {
+        this.uiControl.isMainPage = false;
+      }
+    });
+  }
+
+  componentWillUnmount(): void {
+    if (!this.unListen) {
+      return;
+    }
+
+    this.unListen();
+  }
+}
+
+export const AppRoutes = withRouter(TAppRoutes);
