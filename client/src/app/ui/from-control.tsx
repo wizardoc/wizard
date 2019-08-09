ed29ff3df10112a63430782da727c7998674d436
HaoDaWang <1306036576@qq.com>
2019-08-09 11:07:38 +0800

    [Feat] complete validator

--- client/src/app/ui/from-control.tsx
+++ client/src/app/ui/from-control.tsx
@@ -1,181 +1,114 @@
-import {action, observable} from 'mobx';
+import {FormHelperText} from '@material-ui/core';
+import {observable} from 'mobx';
 import {observer} from 'mobx-react';
-import React, {
-  Children,
-  Component,
-  ReactElement,
-  ReactNode,
-  cloneElement,
-} from 'react';
-import styled from 'styled-components';
+import React, {Component, ReactElement, ReactNode, cloneElement} from 'react';
 
-import {TipStore} from '../store';
-import {InjectStore} from '../utils';
+type Validator = (
+  value: string,
+  name: string,
+  cb: (msg?: string) => void,
+) => void;
 
-import {FormTextFieldProps} from './form-text-field';
-import {Tip, TipVariant} from './tip';
-
-type Validator = (str: string) => true | string;
-type DefaultValidator = 'required';
-type Trigger = 'onChange' | 'onBlur' | 'onInput' | 'onFocus' | string;
-export type FullValidator = () => boolean;
-
-export interface ChangeEvent {
-  target: {value: string};
+interface RuleBody {
+  errMsg?: string;
+  trigger?: string;
+  validator?: Validator;
 }
 
-export interface FormInfo {
-  [index: string]: string;
+interface Rule {
+  [index: string]: RuleBody;
 }
 
-export interface Rule {
-  validator: Validator | DefaultValidator;
-  trigger?: Trigger;
+interface FormControlProps {
+  rules: Rule;
+  onFormDataChange(formData: unknown): void;
 }
 
-export interface Rules {
-  [index: string]: Rule | undefined;
+interface FieldInfo {
+  errMsg: string;
 }
-
-export interface FormControlProps {
-  rules: Rules;
-  children: ReactNode[];
-  onDataUpdate?(formInfo: FormInfo): void;
-  getValidator?(validator: FullValidator): unknown;
-}
-
-const Wrapper = styled.div`
-  width: 100%;
-  flex-direction: column;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const getActuallyValidator = (
-  validator: Validator | DefaultValidator,
-  label: unknown,
-): Validator =>
-  validator === 'required'
-    ? (str: string) => (!!str && str !== '') || `${label} 为必填字段`
-    : validator;
 
 @observer
-export class FromControl extends Component<FormControlProps> {
-  private formInfo: FormInfo = {};
-
+export class FormControl extends Component<FormControlProps> {
   @observable
-  private message: string = '';
-
-  @observable
-  private tipVariant: TipVariant = TipVariant.Success;
-
-  @observable
-  private partProps: FormTextFieldProps[] = [];
-
-  @InjectStore(TipStore)
-  private tipStore!: TipStore;
-
-  @action
-  openTip(message: string, tipVariant: TipVariant): void {
-    this.message = message;
-    this.tipVariant = tipVariant;
-    this.tipStore.tipToggle();
-  }
+  private fieldInfos: {[index: string]: FieldInfo} = {};
 
   render(): ReactNode {
-    const {children, rules, onDataUpdate, getValidator} = this.props;
-    const partProps: FormTextFieldProps[] = [];
+    const {children, rules} = this.props;
 
-    const formParts = Children.map(children, (child, index) => {
-      const node = cloneElement(child as ReactElement);
+    if (!children) {
+      return <></>;
+    }
 
-      partProps[index] = {...node.props};
-
-      const {name, label} = partProps[index];
-      const rule = rules[name];
-
-      if (rule) {
-        const {trigger = 'onChange', validator} = rule;
-        let listener: (...args: unknown[]) => void = () => {};
-
-        if (partProps[index][trigger]) {
-          listener = partProps[index][trigger];
-        }
-
-        partProps[index][trigger] = (e: ChangeEvent) => {
-          const {
-            target: {value},
-          } = e;
-
-          const actuallyValidator: Validator = getActuallyValidator(
-            validator,
-            label,
-          );
-
-          const result = actuallyValidator(value);
-
-          if (typeof result === 'string') {
-            partProps[index].error = true;
-
-            this.openTip(result, TipVariant.Error);
-          } else {
-            partProps[index].error = false;
-
-            this.formInfo[name] = value;
-
-            // process
-            if (onDataUpdate) {
-              onDataUpdate(this.formInfo);
-            }
-          }
-
-          listener(e);
-          this.partProps[index] = partProps[index];
-        };
+    const parts = (children as ReactElement[]).map(child => {
+      if (!child.props) {
+        return <></>;
       }
 
-      return cloneElement(
-        child as ReactElement,
-        this.partProps[index] || partProps[index],
+      let part = cloneElement(child, {
+        ...child.props,
+        'aria-describedby': 'component-error-text',
+      });
+      const {name} = part.props || {name: ''};
+      const rule = rules[name];
+
+      // no rule
+      if (!rule) {
+        return part;
+      }
+
+      let isError = false;
+      const {errMsg, trigger = 'change'} = rule;
+      const listenerName = `on${trigger[0].toUpperCase()}${trigger.slice(1)}`;
+      const setErrMsg = (msg?: string): void => {
+        const info = this.fieldInfos[name] || {};
+
+        info.errMsg = msg || '';
+
+        this.fieldInfos[name] = info;
+      };
+
+      const errorThrower = (msg?: string): void => {
+        setErrMsg(msg);
+
+        isError = true;
+      };
+      const validator: Validator =
+        rule.validator ||
+        ((value: string, name: string, cb: (errMsg?: string) => void): void => {
+          const throwErrMsg = errMsg || `${name} 不能为空`;
+
+          if (!value || value === '') {
+            cb(throwErrMsg);
+          }
+        });
+      const originListener = part.props[listenerName] || ((): void => {});
+
+      part = cloneElement(child, {
+        ...part.props,
+        [listenerName]: (e: any) => {
+          originListener(e);
+
+          isError = false;
+          validator(e.target.value, name, errorThrower);
+
+          if (!isError) {
+            setErrMsg();
+          }
+        },
+        error: !['', undefined].includes((this.fieldInfos[name] || {}).errMsg),
+      });
+
+      return (
+        <>
+          {part}
+          <FormHelperText id="component-error-text">
+            {(this.fieldInfos[name] || {}).errMsg}
+          </FormHelperText>
+        </>
       );
     });
 
-    if (getValidator) {
-      getValidator(
-        (): boolean => {
-          if (!rules) {
-            return true;
-          }
-
-          let noError = true;
-
-          for (const name of Object.keys(rules)) {
-            const {validator} = rules[name] as Rule;
-            const result = getActuallyValidator(validator, name)(
-              this.formInfo[name] || '',
-            );
-
-            if (typeof result === 'string') {
-              this.tipStore.addTipToQueue(result, 'error');
-              noError = false;
-            }
-          }
-
-          return noError;
-        },
-      );
-    }
-
-    return (
-      <Wrapper>
-        {formParts}
-        <Tip tipVariant={this.tipVariant} message={this.message} />
-      </Wrapper>
-    );
-  }
-
-  componentWillUnmount(): void {
-    this.tipStore.destroy();
+    return <>{parts}</>;
   }
 }
