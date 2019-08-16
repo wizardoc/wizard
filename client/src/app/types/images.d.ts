c1e2f898f40b9b20d4af8ad495bc5132930a9aff
HaoDaWang <1306036576@qq.com>
2019-08-16 12:09:22 +0800

    [Format] by tslint

--- client/src/app/types/images.d.ts
+++ client/src/app/types/images.d.ts
@@ -1,7 +1,13 @@
-declare module '*.svg'
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.jpeg'
-declare module '*.gif'
-declare module '*.bmp'
-declare module '*.tiff'
+declare module '*.svg';
+
+declare module '*.png';
+
+declare module '*.jpg';
+
+declare module '*.jpeg';
+
+declare module '*.gif';
+
+declare module '*.bmp';
+
+declare module '*.tiff';
