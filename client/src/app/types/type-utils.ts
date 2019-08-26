7ab194fd0a804fb09a28f0410db7f484fbada779
HaoDaWang <1306036576@qq.com>
2019-08-26 09:51:49 +0800

    [Feat] add Omit util

--- client/src/app/types/type-utils.ts
+++ client/src/app/types/type-utils.ts
@@ -3,3 +3,7 @@ export type Optional<T> = {[P in keyof T]?: T[P]};
 export interface Dict<T> {
   [index: string]: T;
 }
+
+export type Omit<T, K extends keyof T> = T extends any
+  ? Pick<T, Exclude<keyof T, K>>
+  : never;
