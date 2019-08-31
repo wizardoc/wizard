e4c7f7d91639efd8b9b10ff236b7a2261649e057
HaoDaWang <1306036576@qq.com>
2019-08-31 10:27:38 +0800

    [Feat] add get contributors api

--- client/src/app/services/doc-service.ts
+++ client/src/app/services/doc-service.ts
@@ -1,6 +1,7 @@
 import {Inject, Injectable} from 'react-ts-di';
 
 import {HTTP} from '../api';
+import {ContributorInfo} from '../components';
 import {DOC_API} from '../constant';
 
 @Injectable()
@@ -8,7 +9,21 @@ export class DocService {
   @Inject
   private http!: HTTP;
 
+  private readonly usernames: string[] = [
+    'HaoDaWang',
+    'pp3229292',
+    'mpclyl',
+    'XyyF',
+  ];
+
   getAboutWizard(): string {
     return this.http.get(DOC_API.WIZARD);
+  }
+
+  getContributorAvatars(): ContributorInfo[] {
+    return this.usernames.map(username => ({
+      username,
+      addr: `https://avatars.githubusercontent.com/${username}`,
+    }));
   }
 }
