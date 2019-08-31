eca9a3892b36c79d393aef3ae75e4a824f461487
HaoDaWang <1306036576@qq.com>
2019-08-31 17:06:53 +0800

    [Feat] add unit

--- client/src/app/services/time.ts
+++ client/src/app/services/time.ts
@@ -19,4 +19,7 @@ export class Time {
 
   static readonly Second: TimeUnit = 1000;
   static readonly MilliSecond: TimeUnit = 1;
+  static readonly Minute: TimeUnit = 60 * Time.Second;
+  static readonly Hour: TimeUnit = 60 * Time.Minute;
+  static readonly Day: TimeUnit = 24 * Time.Hour;
 }
