11ad7cf4471161ae8889dd79c9748727b502eb5c
HaoDaWang <1306036576@qq.com>
2019-09-01 16:51:49 +0800

    [Feat] wrap title

--- client/src/app/ui/title.tsx
+++ client/src/app/ui/title.tsx
@@ -1,22 +1,40 @@
-import React, {FunctionComponent} from 'react';
+import {IconButton} from '@material-ui/core';
+import {SvgIconProps} from '@material-ui/core/SvgIcon';
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import React, {ComponentType, FunctionComponent, ReactNode} from 'react';
 import styled from 'styled-components';
 
 interface TitleProps extends BlockProps {}
 
-interface BlockProps {
-  blockColor?: string;
+interface ReverseProps {
+  isReverse?: boolean;
 }
+
+interface HasBackProps {
+  hasBack?: boolean;
+}
+
+interface BlockProps extends ReverseProps, HasBackProps {
+  blockColor?: string;
+  onBackClick?(): void;
+}
+
+interface TextProps extends ReverseProps {}
+
+interface BackButtonProps extends HasBackProps, ReverseProps {}
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
   padding-top: 30px;
+  justify-content: space-between;
 `;
 
-const Text = styled.p`
+const Text = styled.p<TextProps>`
   font-size: 50px;
   font-weight: 100;
-  margin: 0 0 0 30px;
+  margin: 0;
+  ${props => (props.isReverse ? 'margin-right' : 'margin-left')}: 30px;
 `;
 
 const Block = styled.div<BlockProps>`
@@ -25,9 +43,49 @@ const Block = styled.div<BlockProps>`
   background: ${props => props.theme[props.blockColor || 'primaryColor']};
 `;
 
-export const Title: FunctionComponent<TitleProps> = props => (
-  <Wrapper>
-    <Block blockColor={props.blockColor}></Block>
-    <Text>{props.children}</Text>
-  </Wrapper>
-);
+const BackButton = styled(IconButton)<BackButtonProps>`
+  font-size: 50px !important;
+  color: #1976d2;
+  cursor: pointer;
+  visibility: ${props => (props.hasBack ? 'visible' : 'hidden')};
+
+  ${props => (props.isReverse ? 'margin-left' : 'margin-right')}: 20px;
+` as ComponentType<SvgIconProps & BackButtonProps>;
+
+const KeyboardArrowLeftButton = styled(KeyboardArrowLeftIcon)`
+  font-size: 50px !important;
+  cursor: pointer;
+` as ComponentType<SvgIconProps>;
+
+const TextWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Title: FunctionComponent<TitleProps> = props => {
+  const {
+    blockColor,
+    isReverse = false,
+    children,
+    onBackClick = (): void => {},
+    hasBack = false,
+  } = props;
+  const reverse = (arr: ReactNode[]): ReactNode[] =>
+    isReverse ? arr.reverse() : arr;
+  const rawNodes = [
+    <Block blockColor={blockColor}></Block>,
+    <Text isReverse={isReverse}>{children}</Text>,
+  ];
+  const sortedNodes = [
+    <TextWrapper>{...reverse(rawNodes)}</TextWrapper>,
+    <BackButton
+      onClick={(): void => onBackClick()}
+      isReverse={isReverse}
+      hasBack={hasBack}
+    >
+      <KeyboardArrowLeftButton></KeyboardArrowLeftButton>
+    </BackButton>,
+  ];
+
+  return <Wrapper>{...reverse(sortedNodes)}</Wrapper>;
+};
