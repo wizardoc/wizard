3bc45e30be426630d5601212d730c59de0d17989
HaoDaWang <1306036576@qq.com>
2019-09-10 22:09:37 +0800

    [Feat] add some color

--- client/src/app/theme/style.ts
+++ client/src/app/theme/style.ts
@@ -17,10 +17,12 @@ export interface IThemeInterface {
 export const styledTheme = {
   primaryColor: '#1976d2',
   secondaryColor: '#f06292',
+  thirdColor: '#feca57',
   darkPrimaryColor: '#1565c0',
   dark: '#424242',
   highRed: '#e91e63',
   shallowGray: '#eeeeee',
+  deepGray: '#cecece',
   redLinearGradient: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
   spacing: '10px',
   baseBgColor: '#eceff1',
@@ -29,6 +31,9 @@ export const styledTheme = {
   articleColor: '#2f2f2f',
   quoteBgColor: 'rgba(255, 218, 121,0.2)',
   white: '#fff',
+  grayTextColor: 'rgba(0, 0, 0, 0.54)',
+  baseShadow:
+    '0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12);',
 };
 
 export default styled;
