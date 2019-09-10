9b8bd04f726a3a217084451af4e8ea0858308294
HaoDaWang <1306036576@qq.com>
2019-09-10 22:08:58 +0800

    [Refactor] remove redundancy code

--- client/src/app/components/doc-recent-update-drawer.tsx
+++ client/src/app/components/doc-recent-update-drawer.tsx
@@ -1,32 +1,19 @@
-import {List, SwipeableDrawer} from '@material-ui/core';
+import {List} from '@material-ui/core';
 import PetsIcon from '@material-ui/icons/Pets';
 import {observer} from 'mobx-react';
 import React, {Component, ReactNode} from 'react';
 
-import {RecentDrawer} from '../store';
 import {DrawerHeader} from '../ui';
-import {InjectStore} from '../utils';
 
 const HEAD_PROMPT = '显示近期的文档动态（保留 30 天）';
 
 @observer
 export class DocRecentUpdateDrawer extends Component {
-  @InjectStore(RecentDrawer)
-  private recentStore!: RecentDrawer;
-
   render(): ReactNode {
-    const {isViewRecentDrawer, viewRecentDrawerToggle} = this.recentStore;
-
     return (
-      <SwipeableDrawer
-        open={isViewRecentDrawer}
-        onOpen={viewRecentDrawerToggle}
-        onClose={viewRecentDrawerToggle}
-      >
-        <List>
-          <DrawerHeader title={HEAD_PROMPT} icon={<PetsIcon />}></DrawerHeader>
-        </List>
-      </SwipeableDrawer>
+      <List>
+        <DrawerHeader title={HEAD_PROMPT} icon={<PetsIcon />}></DrawerHeader>
+      </List>
     );
   }
 }
