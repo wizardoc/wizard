8921c586606682f81421cd95efc8d2672d802915
HaoDaWang <1306036576@qq.com>
2019-09-26 21:41:36 +0800

    patch: fix some bug for ui component

--- client/src/app/ui/input/with-icon.tsx
+++ client/src/app/ui/input/with-icon.tsx
@@ -1,27 +1,67 @@
 import {TextField} from '@material-ui/core';
 import {TextFieldProps} from '@material-ui/core/TextField';
+import {observable} from 'mobx';
+import {observer} from 'mobx-react';
 import React, {Component, ReactNode} from 'react';
 import styled from 'styled-components';
 
-interface WithIconInputProps {
+interface IconProps {
+  iconSize?: string;
+  focusColor?: string;
+}
+
+interface FocusIconProps {
+  isFocus?: boolean;
+}
+
+interface IconInputProps extends IconProps {
   icon: ReactNode;
 }
+
+export type WithIconInputProps = TextFieldProps & IconInputProps;
 
 const Wrapper = styled.div`
   width: fit-content;
   height: fit-content;
   display: flex;
-  align-items: center;
+  align-items: flex-end;
 `;
 
-export class WithIconInput extends Component<
-  TextFieldProps & WithIconInputProps
-> {
+const Icon = styled.div<IconProps & FocusIconProps>`
+  margin-right: 10px;
+
+  > svg {
+    ${props =>
+      props.isFocus &&
+      `color: ${props.focusColor || props.theme.primaryColor};`}
+    ${props => (props.iconSize ? `font-size:${props.iconSize}` : '')};
+  }
+`;
+
+@observer
+export class WithIconInput extends Component<WithIconInputProps> {
+  @observable
+  isTextFieldFocus = false;
+
+  toggleIsTextFieldFocus(): void {
+    this.isTextFieldFocus = !this.isTextFieldFocus;
+  }
+
   render(): ReactNode {
     return (
       <Wrapper>
-        {this.props.icon}
-        <TextField {...this.props}></TextField>
+        <Icon
+          iconSize={this.props.iconSize}
+          focusColor={this.props.focusColor}
+          isFocus={this.isTextFieldFocus}
+        >
+          {this.props.icon}
+        </Icon>
+        <TextField
+          {...this.props}
+          onFocus={() => this.toggleIsTextFieldFocus()}
+          onBlur={() => this.toggleIsTextFieldFocus()}
+        ></TextField>
       </Wrapper>
     );
   }
