964e996f93100723095d164c4f577397c1c5045c
youncccat <1306036576@qq.com>
2020-05-17 15:48:44 +0800

    [Refactor] avoid render repeat

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,11 +2,11 @@ import {MuiThemeProvider} from '@material-ui/core/styles';
 // import EmailIcon from '@material-ui/icons/Email';
 import {observer} from 'mobx-react';
 import {SnackbarProvider, WithSnackbarProps, withSnackbar} from 'notistack';
-import React, {Component, FunctionComponent} from 'react';
+import React, {Component, FunctionComponent, ReactNode} from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {Inject} from 'react-ts-di';
 
-import {CommonDialog, OptionalTip, Profile} from './components';
+import {CommonDialog, OptionalTip, Profile, Default} from './components';
 import {DrawerService, DialogPool, OptionalTipService} from './services';
 import {TipStore} from './store';
 import {GlobalStyle, ThemeProvider, styledTheme, theme} from './theme';
@@ -17,41 +17,68 @@ import {AppRouting} from './app-routing';
 const MAX_SNACK_BAR_COUNT = 3;
 
 @observer
+class DialogEntry extends Component {
+  @Inject
+  dialogPool!: DialogPool;
+
+  render(): ReactNode {
+    console.info(Array.from(this.dialogPool.dialogs.values()));
+
+    return Array.from(this.dialogPool.dialogs.keys()).map(dialogID => (
+      <CommonDialog key={dialogID} dialogID={dialogID}></CommonDialog>
+    ));
+  }
+}
+
+@observer
+class OptionalTipEntry extends Component {
+  @Inject
+  optionalTipService!: OptionalTipService;
+
+  render(): ReactNode {
+    return this.optionalTipService.tipInfos.map(info => (
+      <OptionalTip key={info.name} {...info}></OptionalTip>
+    ));
+  }
+}
+
+@observer
+class DrawerEntry extends Component {
+  @Inject
+  drawerService!: DrawerService;
+
+  render(): ReactNode {
+    const {options, isShow, currentDrawer} = this.drawerService;
+
+    return (
+      <Default condition={() => !currentDrawer}>
+        <Drawer {...options} open={isShow}>
+          {currentDrawer}
+        </Drawer>
+      </Default>
+    );
+  }
+}
+
+@observer
 class TApp extends Component<WithSnackbarProps> {
   @InjectStore(TipStore)
   tipStore!: TipStore;
 
-  @Inject
-  dialogPool!: DialogPool;
-
-  @Inject
-  optionalTipService!: OptionalTipService;
-
-  @Inject
-  drawerService!: DrawerService;
-
   render(): React.ReactNode {
-    const {options, isShow, currentDrawer} = this.drawerService;
+    console.info('top render');
 
     return (
       <ThemeProvider theme={styledTheme}>
         <BrowserRouter>
           <MuiThemeProvider theme={theme}>
             {/* Drawer is render by service here */}
-            {currentDrawer && (
-              <Drawer {...options} open={isShow}>
-                {currentDrawer}
-              </Drawer>
-            )}
+            <DrawerEntry />
             {/* Dialog is render by service here */}
-            {Array.from(this.dialogPool.dialogs.keys()).map(dialogID => (
-              <CommonDialog key={dialogID} dialogID={dialogID}></CommonDialog>
-            ))}
+            <DialogEntry />
             <GlobalStyle />
             <Profile />
-            {this.optionalTipService.tipInfos.map(info => (
-              <OptionalTip key={info.name} {...info}></OptionalTip>
-            ))}
+            <OptionalTipEntry />
             {/* <HeaderBar /> */}
             <AppRouting />
           </MuiThemeProvider>
