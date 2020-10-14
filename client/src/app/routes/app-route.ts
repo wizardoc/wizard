a3c445ff86176e460a4f04fbc8f731bd887f6f60
youncccat <1306036576@qq.com>
2020-10-14 21:27:34 +0800

    [Remove] redundancy code

--- client/src/app/routes/app-route.ts
+++ client/src/app/routes/app-route.ts
@@ -3,9 +3,6 @@ import {lazy} from 'src/app/utils';
 import {Routes} from '../services';
 import {Home} from '../pages/home';
 
-/** Lazy load */
-// const Home = lazy(import('../pages/home'), 'Home');
-
 const Organization = lazy(import('../pages/organization'), 'Organization');
 
 export const RootRoutes: Routes = [
