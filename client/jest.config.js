abb3c6e59c8d01d087bc0f3a1efc51c491811890
youncccat <1306036576@qq.com>
2020-12-08 15:27:15 +0800

    [Fix] fix import path of babel-jest

--- client/jest.config.js
+++ client/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   testEnvironment: 'node',
   testURL: 'http://localhost',
   transform: {
-    '^.+\\.(js|jsx|mjs)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(js|jsx|mjs)$': 'babel-jest',
     '^.+\\.tsx?$': '<rootDir>/config/jest/typescriptTransform.js',
     '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
     '^(?!.*\\.(js|jsx|mjs|css|json)$)':
