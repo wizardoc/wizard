da9694e81ca1299a82332932bf548933d39cd024
noob-injoker <1769031928@qq.com>
2021-01-13 22:25:58 +0800

    [Test]  title

--- client/src/website/pages/home.tsx
+++ client/src/website/pages/home.tsx
@@ -1,8 +1,8 @@
-import React, {Component, ReactNode} from 'react';
+import React, { Component, ReactNode } from 'react';
 // tslint:disable-next-line:import-path-shallowest
 import styled from 'styled-components';
 
-import {RouterAnimation} from '../animations';
+import { RouterAnimation } from '../animations';
 import {
   MainContent,
   Started,
@@ -24,11 +24,11 @@ export class Home extends Component {
         <Started />
         <MainContent />
         <ViewBanner
-          title="基于组织的知识管理"
+          title="基于组织的知识管理ceng"
           description="团队的知识管理，享受历史追溯，Diff 通知，让更多博主收益其中。"
           actionButtons={[
-            {text: '马上加入', primary: true, handler() {}},
-            {text: '先去看看', handler() {}},
+            { text: '马上加入', primary: true, handler() { } },
+            { text: '先去看看', handler() { } },
           ]}
         ></ViewBanner>
         <GraphicContainer
@@ -46,5 +46,5 @@ export class Home extends Component {
     );
   }
 
-  componentWillUpdate(): void {}
+  componentWillUpdate(): void { }
 }
