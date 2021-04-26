125d30c7ae10ed60a25b278e7e9caf7a8149d824
youncccat <1306036576@qq.com>
2021-04-26 22:57:45 +0800

    [Chore] placehold ts folder & add comments for create service instance

--- packages/http-request/example/js/main.js
+++ packages/http-request/example/js/main.js
@@ -1,14 +1,18 @@
-import {GitHubService} from "./github-service"
+import {GitHubService} from './github-service';
 
-const githubService = new GitHubService()
+const MOCK_USERNAME = 'youncccat';
 
-async function main(){
-  const MOCK_USERNAME = "youncccat"
-  const res = await githubService.getUserDetail(MOCK_USERNAME)
+async function main() {
+  // The service might inject in class as well as bound a certain variable using
+  // DI lib that implemented by metadata in particular condition
+  const githubService = new GitHubService();
+  const res = await githubService.getUserDetail(MOCK_USERNAME);
 
-  const {data:avatarURL} = res.expect(() => `Cannot access ${MOCK_USERNAME} information`).pipe(data => data?.avatar_url)
+  const {data: avatarURL} = res
+    .expect(() => `Cannot access ${MOCK_USERNAME} information`)
+    .pipe(data => data?.avatar_url);
 
-  console.info(`${MOCK_USERNAME}'s avatar is ${avatarURL}`)
+  console.info(`${MOCK_USERNAME}'s avatar is ${avatarURL}`);
 }
 
 main();
